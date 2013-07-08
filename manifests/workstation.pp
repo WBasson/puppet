1e78ae5c0c53eb4771d07dd57d362898c81d1bce
Tom Bamford <tb@hetzner.co.za>
2013-07-08 09:39:10 +0200

    Conditionalise package names and workstation username

--- manifests/workstation.pp
+++ manifests/workstation.pp
@@ -3,18 +3,29 @@ Exec {
   timeout => 0,
 }
 
-include chrome
+$username = $::hostname ? {
+  /hamlet(.+)/   => 'tom',
+  'tomb-desktop' => 'tomb',
+}
+
 include clitools
 include devtools
 include kerberos
 include openssh
-include skype
 include stdlib
+
+if $::operatingsystem == 'Fedora' {
+  include chrome
+  include skype
+}
 
 package { 'clementine': ensure => installed }
 package { [ 'clusterssh', 'pssh' ]: ensure => installed }
 package { 'elinks': ensure => installed }
-package { 'firefox': ensure => installed }
+package { $::operatingsystem ? {
+  'Debian' => 'iceweasel',
+  'Fedora' => 'firefox',
+}: ensure => installed }
 package { [ 'gimp', 'gimp-data-extras' ]: ensure => installed }
 package { 'git': ensure => installed }
 package { 'inkscape': ensure => installed }
@@ -22,13 +33,22 @@ package { 'java-1.7.0-openjdk': ensure => installed }
 package { 'mcollective-client': ensure => installed }
 package { 'puppet': ensure => installed }
 package { 'qmpdclient': ensure => installed }
-package { [ 'remmina', 'remmina-plugins-nx', 'remmina-plugins-vnc' ]: ensure => installed }
+package { $::operatingsystem ? {
+  'Debian' => [ 'remmina', 'remmina-plugin-nx', 'remmina-plugin-vnc' ],
+  'Fedora' => [ 'remmina', 'remmina-plugins-nx', 'remmina-plugins-vnc' ],
+}: ensure => installed }
 package { [ 'ruby', 'rubygems' ]: ensure => installed }
 package { [ 'screen', 'tmux' ]: ensure => installed }
 package { 'synergy': ensure => installed }
-package { 'thunderbird': ensure => installed }
+package { $::operatingsystem ? {
+  'Debian' => 'icedove',
+  'Fedora' => 'thunderbird',
+}: ensure => installed }
 package { 'vim-enhanced': ensure => installed }
-package { 'wireshark-gnome': ensure => installed }
+package { $::operatingsystem ? {
+  'Debian' => 'wireshark',
+  'Fedora' => 'wireshark-gnome',
+}: ensure => installed }
 package { 'zsh': ensure => installed }
 
 $vboxgroup = $::virtual ? {
@@ -36,14 +56,14 @@ $vboxgroup = $::virtual ? {
   'virtualbox' => 'vboxsf',
 }
 
-user { 'tom':
+user { $username:
   groups  => [ 'dialout', 'wheel', $vboxgroup, ],
   home    => '/home/tom',
   shell   => '/bin/zsh',
   require => Package['zsh'],
 }
 
-class { 'kde::autologin': user => 'tom' }
+class { 'kde::autologin': user => $username }
 
 users::dotfile { [
   'aliases',
@@ -56,22 +76,22 @@ users::dotfile { [
   'shell_colours',
   'vimrc',
   'zshrc',
-]: user => 'tom' }
-users::dotfile { [ 'ssh/config', 'ssh/known_hosts' ]: user => 'tom', mode => '0600' }
-users::dotfile { 'zsh': user => 'tom', recurse => true }
+]: user => $username }
+users::dotfile { [ 'ssh/config', 'ssh/known_hosts' ]: user => $username, mode => '0600' }
+users::dotfile { 'zsh': user => $username, recurse => true }
 
 if $::virtual == 'physical' {
-  users::dropbox { 'tom': }
+  users::dropbox { $username: }
 }
 
-users::kdeconfig { 'app_launcher_shortcut': user => 'tom', file => 'kglobalshortcutsrc', group => 'plasma-desktop', key => 'activate widget 2', value => 'Alt+F1,Alt+F1,Activate Application Launcher Widget' }
-users::kdeconfig { 'disable_compositing': user => 'tom', file => 'kwinrc', group => 'Compositing', key => 'Enabled', value => false }
-users::kdeconfig { 'font_aliasing': user => 'tom', group => 'General', key => 'XftAntialias', value => 'true' }
-users::kdeconfig { 'font_aliasing_hinting': user => 'tom', group => 'General', key => 'XftHintStyle', value => 'slight' }
-users::kdeconfig { 'font_aliasing_subpixel': user => 'tom', group => 'General', key => 'XftSubPixel', value => 'rgb' }
-users::kdeconfig { 'oxygen_theme': user => 'tom', file => 'plasmarc', group => 'Theme', key => 'name', value => 'oxygen' }
+users::kdeconfig { 'app_launcher_shortcut': user => $username, file => 'kglobalshortcutsrc', group => 'plasma-desktop', key => 'activate widget 2', value => 'Alt+F1,Alt+F1,Activate Application Launcher Widget' }
+users::kdeconfig { 'disable_compositing': user => $username, file => 'kwinrc', group => 'Compositing', key => 'Enabled', value => false }
+users::kdeconfig { 'font_aliasing': user => $username, group => 'General', key => 'XftAntialias', value => 'true' }
+users::kdeconfig { 'font_aliasing_hinting': user => $username, group => 'General', key => 'XftHintStyle', value => 'slight' }
+users::kdeconfig { 'font_aliasing_subpixel': user => $username, group => 'General', key => 'XftSubPixel', value => 'rgb' }
+users::kdeconfig { 'oxygen_theme': user => $username, file => 'plasmarc', group => 'Theme', key => 'name', value => 'oxygen' }
 
-#users::rvm { 'tom': }
+#users::rvm { $username: }
 
 users::script { [
   'askpass.sh',
@@ -81,9 +101,9 @@ users::script { [
   's',
   'ssh-add-keys.sh',
   'steam_bootstrap.sh',
-]: user => 'tom' }
+]: user => $username }
 
-users::vim { 'tom': }
+users::vim { $username: }
 users::vimbundle { [
   'colorschemes',
   'fugitive',
@@ -92,4 +112,4 @@ users::vimbundle { [
   'rails',
   'ruby',
   'syntastic',
-]: user => 'tom' }
+]: user => $username }
