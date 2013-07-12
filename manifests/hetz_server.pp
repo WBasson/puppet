bbbd90de8673563858f8a68cf210bfb6ffc8e001
Tom Bamford <tb@hetzner.co.za>
2013-07-12 14:42:09 +0200

    Manifest for Hetzner managed servers

---
+++ manifests/hetz_server.pp
@@ -0,0 +1,47 @@
+Exec {
+  path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+  timeout => 0,
+}
+
+$username = 'tom.b'
+
+#package { 'zsh': ensure => installed }
+
+#user { $username:
+#  shell   => '/bin/zsh',
+#  require => Package['zsh'],
+#}
+
+users::dotfile { [
+  'aliases',
+  'bashrc',
+  'env',
+  'gitconfig',
+  'gitignore_global',
+  'profile',
+  'screenrc',
+  'shell_colours',
+  'vimrc',
+  'zshrc',
+]: user => $username }
+users::dotfile { [ 'ssh/config', 'ssh/known_hosts' ]: user => $username, mode => '0600' }
+users::dotfile { 'zsh': user => $username, recurse => true }
+
+users::script { [
+  'env-push.sh',
+  'fix_known_hosts.rb',
+  'hetz-qualify-hostname.sh',
+  's',
+  'ssh-add-keys.sh',
+]: user => $username }
+
+users::vim { $username: }
+users::vimbundle { [
+  'colorschemes',
+  'fugitive',
+  'git',
+  'puppet',
+  'rails',
+  'ruby',
+  'syntastic',
+]: user => $username }
