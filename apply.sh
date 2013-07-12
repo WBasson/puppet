75013f84cddf5c98a3210f903d8b73263c097a8c
Tom Bamford <tb@hetzner.co.za>
2013-07-12 15:10:55 +0200

    Split submodules update into own script

--- apply.sh
+++ apply.sh
@@ -12,19 +12,21 @@ else
 fi
 
 # Initialise git submodules
-git submodule status | egrep -q '^-[0-9a-zA-Z]+' && git submodule update --init
+[ $(id -u) -ne 0 ] && ./submodules.sh
 
 # Copy updated ssh/known_hosts back into repository and commit (if no other changes are staged)
-KNOWN_HOSTS_REAL="$HOME/.ssh/known_hosts"
-KNOWN_HOSTS_REPO="$DIR/modules/users/files/$USER/dotfiles/ssh/known_hosts"
-if [ -f "$KNOWN_HOSTS_REAL" ] && [ "$KNOWN_HOSTS_REAL" -nt "$KNOWN_HOSTS_REPO" ]; then
-  SUM_REAL=$(md5sum "$KNOWN_HOSTS_REAL" | awk '{print $1;}')
-  SUM_REPO=$(md5sum "$KNOWN_HOSTS_REPO" | awk '{print $1;}')
-  if [ "$SUM_REPO" != "$SUM_REAL" ]; then
-    cp -v "$KNOWN_HOSTS_REAL" "$KNOWN_HOSTS_REPO"
-    if ! git status | grep -qc '^# Changes to be committed:$'; then
-      git add "$KNOWN_HOSTS_REPO"
-      git commit -m 'Update ssh/known_hosts'
+if [ "$MANIFEST" == "workstation" ]; then
+  KNOWN_HOSTS_REAL="$HOME/.ssh/known_hosts"
+  KNOWN_HOSTS_REPO="$DIR/modules/users/files/$USER/dotfiles/ssh/known_hosts"
+  if [ -f "$KNOWN_HOSTS_REAL" ] && [ "$KNOWN_HOSTS_REAL" -nt "$KNOWN_HOSTS_REPO" ]; then
+    SUM_REAL=$(md5sum "$KNOWN_HOSTS_REAL" | awk '{print $1;}')
+    SUM_REPO=$(md5sum "$KNOWN_HOSTS_REPO" | awk '{print $1;}')
+    if [ "$SUM_REPO" != "$SUM_REAL" ]; then
+      cp -v "$KNOWN_HOSTS_REAL" "$KNOWN_HOSTS_REPO"
+      if ! git status | grep -qc '^# Changes to be committed:$'; then
+        git add "$KNOWN_HOSTS_REPO"
+        git commit -m 'Update ssh/known_hosts'
+      fi
     fi
   fi
 fi
