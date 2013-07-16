906afc5eb33cde506d6c0a35657956fe3bc2339e
Tom Bamford <tb@hetzner.co.za>
2013-07-16 13:30:35 +0200

    Fix return status in submodules.sh

--- submodules.sh
+++ submodules.sh
@@ -3,8 +3,10 @@
 # Determine correct directory and change into it
 cd "$(cd "$(dirname "$0")" && pwd)"
 
-# Initialise git submodules
-git submodule status | egrep -q '^-[0-9a-zA-Z]+' && git submodule update --init
-
-# Exit with status code
-exit $?
+# Initialise git submodules and return status code
+if git submodule status | egrep -q '^-[0-9a-zA-Z]+'; then
+  git submodule update --init
+  exit $?
+else
+  exit 0
+fi
